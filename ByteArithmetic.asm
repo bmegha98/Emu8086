b88c96fec4f0aabdcaa2b55ac3352b78cd34afd6
Megha Bisht <bishtmegha98@gmail.com>
2018-09-16 18:48:19 +0530

    Adding Byte Variables

---
+++ ByteArithmetic.asm
@@ -0,0 +1,36 @@
+; OBJECTIVE : BYTE ARITHMETIC
+
+DATA SEGMENT  
+    
+; VARIABLES INITIALIZATION
+
+    X DB 12H
+    Y DB 43H
+    Z DB 54H
+    result DB ?
+    
+DATA ENDS 
+
+CODE SEGMENT  
+    
+    ASSUME CS:CODE  DS:DATA 
+    
+    START:  
+  
+ ; MOVING ADDRESS OF DATA TO DS(DATA SEGMENT)
+     
+     MOV AX,DATA
+     MOV DS,AX
+  
+  ; EVALUATING EXPRESSION X+Y+24-Z
+    
+     MOV AL,X
+     ADD AL,Y
+     ADD AL,24H
+     SUB AL,Z
+     MOV result,AL        ; Final Result is in result variable
+     
+CODE ENDS
+
+END START
+    
